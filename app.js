0257bb5dc128421e5e79848bef15b74c892f634e
Jondy Zhao <jondy.zhao@gmail.com>
2017-09-26 10:59:06 +0800

    Use min.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ require.config( {
 
     paths: {
         ol: 'lib/ol-4.3.2/ol',
-        easyrtc: 'lib/easyrtc',
-        dexie: 'lib/dexie-1.5.1/dexie',
+        easyrtc: 'lib/easyrtc.min',
+        dexie: 'lib/dexie-1.5.1/dexie.min',
         three: 'lib/three.min',
         'socket.io': 'lib/socket.io.min',
     }
