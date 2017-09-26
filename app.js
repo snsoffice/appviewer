4f5a169b3259b4c4b37b3bdf942782a31b1c622e
Jondy Zhao <jondy.zhao@gmail.com>
2017-09-26 10:00:39 +0800

    Add functions

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-require.config({
+require.config( {
+
     baseUrl: 'js',
+
     paths: {
         ol: 'lib/ol-4.3.2/ol',
-        dexie: 'lib/dexie-1.5.1/dexie'
+        easyrtc: 'lib/easyrtc',
+        dexie: 'lib/dexie-1.5.1/dexie',
+        three: 'lib/three.min',
+        'socket.io': 'lib/socket.io.min',
     }
-});
 
-requirejs(['main']);
+} );
+
+requirejs( [ 'main' ] );
--- js/main.js
+++ js/main.js
@@ -1,11 +1,57 @@
-requirejs(['dexie', 'ol'], function(dexie, ol) {
+requirejs(['dexie', 'ol', 'easyrtc', 'utils'], function(dexie, ol, easyrtc, utils) {
 
-    var globalMessage = document.getElementById('global-message');
-    var showMessage = function (msg) {
-        globalMessage.innerHTML = '<div class="alert alert-warning" role="alert">' +
-            '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
-            '<strong>Warning!</strong> ' + msg + '</div>';
-        globalMessage.style.display = 'block';
+    document.addEventListener('look', function (e) {
+        console.log('look'); 
+    }, false);
+    var evt = new Event("look", {"bubbles": false, "cancelable": false});
+    document.dispatchEvent(evt);
+
+    easyrtc.setSocketUrl("http://snsoffice.com:9090");
+    // easyrtc.connect("yuanjing", function (rid, room) { showMessage('rid:' + rid + ', room is ' + room); }, function (e, msg) { showMessage('fail:' + msg); } );
+
+    if (navigator.onLine) {
+        console.log('online');
+    } else {
+        console.log('offline');
+    }
+
+    window.addEventListener('offline', function(e) {alert('offline');});
+    window.addEventListener('online', function(e) {alert('online``');});
+
+    window.localStorage.setItem('yuanjing_username', 'anonymous');
+    window.localStorage.clear();
+
+    var loadBingsMapLayers = function () {
+        var center = ol.proj.fromLonLat([108.54, 34.16]); // Xi'an China
+        var zoom = 8;
+        var culture = 'zh-cn';
+        if (typeof style === 'undefined')
+            style = 'AerialWithLabels';
+
+        var styles = [
+            'Road',
+            'Aerial',
+            'AerialWithLabels',
+            'collinsBart',
+            'ordnanceSurvey'
+        ];
+        var layers = [];
+        var i, ii;
+        for (i = 0, ii = styles.length; i < ii; ++i) {
+            layers.push(new ol.layer.Tile({
+                visible: false,
+                preload: Infinity,
+                title: styles[i],
+                source: new ol.source.BingMaps({
+                    key: 'AtHtvweLfmjJag2BTGXsX0kW-2ExduYJXOU-78cgNz4Y_m7UylYgMmfbEwlYyPPb',
+                    imagerySet: styles[i],
+                    culture: culture
+                    // use maxZoom 19 to see stretched tiles instead of the BingMaps
+                    // "no photos at this zoom level" tiles
+                    // maxZoom: 19
+                })
+            }));
+        }
     };
 
     var location = [-251.03894817,  34.22705742];
@@ -13,6 +59,47 @@ requirejs(['dexie', 'ol'], function(dexie, ol) {
     var layer = new ol.layer.Tile({
         source: new ol.source.OSM()
     });
+    var bingLayer = new ol.layer.Tile({
+        preload: Infinity,
+        title: 'Aerial',
+        source: new ol.source.BingMaps({
+            key: 'AtHtvweLfmjJag2BTGXsX0kW-2ExduYJXOU-78cgNz4Y_m7UylYgMmfbEwlYyPPb',
+            imagerySet: 'Aerial',
+            culture: 'zh-cn'
+            // use maxZoom 19 to see stretched tiles instead of the BingMaps
+            // "no photos at this zoom level" tiles
+            // maxZoom: 19
+        })
+    });
+    var gdLayer = new ol.layer.Tile({
+        source: new ol.source.XYZ({
+            crossOrigin: 'anonymous',
+            url:'http://webst0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}'
+        })
+    });
+
+    var bingChineseLayer = new ol.layer.Tile({
+        source: new ol.source.XYZ({
+            crossOrigin: 'anonymous',
+            tilePixelRatio: 2,
+            tileUrlFunction: function(tileCoord){
+                var z = tileCoord[0];
+                var x = tileCoord[1];
+                var y = -tileCoord[2] - 1;
+                var result='', zIndex=0;
+
+                for(; zIndex<z; zIndex++) {
+                    result = ((x&1)+2*(y&1)).toString() + result;
+                    x >>= 1;
+                    y >>= 1;
+                }
+                return 'http://dynamic.t0.tiles.ditu.live.com/comp/ch/' + result + '?it=G,VE,BX,L,LA&mkt=zh-cn,syr&n=z&og=111&ur=CN';
+            }
+        })
+    });
+
+    layer = gdLayer;
+
     var map = new ol.Map({
         target: 'map',
         layers: [ layer ],
@@ -26,10 +113,11 @@ requirejs(['dexie', 'ol'], function(dexie, ol) {
         document.getElementById('splash').remove();
     });
 
-    var navbar = document.getElementById('navbar');
-    Array.prototype.forEach.call(navbar.getElementsByClassName('navbar-brand'), function (element) {
-        element.addEventListener('click', function (e) {
-            document.getElementById('dashboard').style.display = 'block';
+    var navbar = document.getElementById( 'navbar' );
+
+    Array.prototype.forEach.call( navbar.getElementsByClassName( 'navbar-brand' ), function ( element ) {
+        element.addEventListener( 'click', function ( e ) {
+            document.getElementById( 'dashboard' ).style.display = 'block';
             e.preventDefault();
         }, false);
     });
@@ -54,7 +142,7 @@ requirejs(['dexie', 'ol'], function(dexie, ol) {
         element.addEventListener('mousedown', function (e) {
             if (document.hasFocus() && document.activeElement === e.target && searchResults.hasChildNodes() && searchResults.style.display === 'none') {
                 searchResults.style.display = 'block';
-                e.stopPropagation();
+                // e.stopPropagation();
             }
         }, false);
     });
@@ -68,7 +156,8 @@ requirejs(['dexie', 'ol'], function(dexie, ol) {
     };
 
     var dashboard = document.getElementById('dashboard');
-    dashboard.firstElementChild.nextElementSibling.addEventListener('mousedown', function (e) {
+    dashboard.firstElementChild.nextElementSibling.addEventListener('click', function (e) {
+        e.preventDefault();
         if (e.target.tagName === 'A') {
             var li = e.target.parentElement;
             if (li.className !== 'active') {
@@ -81,14 +170,47 @@ requirejs(['dexie', 'ol'], function(dexie, ol) {
                 var tabname = li.getAttribute('tab-name');
                 dashboard.lastElementChild.querySelector(tabname).className += ' active';
             }
-            e.stopPropagation();
-            e.preventDefault();
+            // e.stopPropagation();
         }
     }, false);
 
-    document.addEventListener('mousedown', function (e) {
-        searchResults.style.display = 'none';
-        dashboard.style.display = 'none';
-        document.getElementById('global-message').style.display = 'none';
+    // dashboard.firstElementChild.nextElementSibling.addEventListener('click', function (e) {
+    //     
+    // });
+
+    // document.getElementById('login').addEventListener('mousedown', function (e) {
+    //     var request = new XMLHttpRequest();
+    //     request.onerror = function ( event ) {
+    //         alert( 'Error: ' + event.toString() );
+    //     };
+
+    //     request.onloadend = function() {
+
+    //         if (request.status != 200) {
+    //             alert( 'status is not 200, but ' + request.status);
+    //             return;
+    //         }
+
+    //         alert ( 'Got response: ' + request.responseText);
+    //     };
+    //     var url = 'http://snsoffice.com:9098/house/applogin';
+    //     request.open('GET', url, true);        
+    //     request.send();
+    //     e.stopPropagation();
+    // }, false);
+
+    document.addEventListener( 'click', function ( e ) {
+
+        var tag = e.target.tagName;
+
+        if ( ( ! ( tag === 'BUTTON' || tag === 'A' ) ) || ( e.target.className === 'close' ) )
+            dashboard.style.display = 'none';
+
+        if ( ! ( tag === 'INPUT' && e.target.name === 'search' ) )
+             searchResults.style.display = 'none';
+
+        document.getElementById( 'global-message' ).style.display = 'none';
+
     }, false);
+
 });
