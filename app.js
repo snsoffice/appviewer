3e6b03720e7c931f6d71f44a1dddb5bcef23dcf8
Jondy Zhao <jondy.zhao@gmail.com>
2017-10-02 09:47:58 +0800

    Use amd for all js

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ require.config( {
     baseUrl: 'js',
 
     paths: {
+        jquery: 'lib/jquery.min',
         ol: 'lib/ol-4.3.2/ol',
         easyrtc: 'lib/easyrtc.min',
         dexie: 'lib/dexie-1.5.1/dexie.min',
         three: 'lib/three.min',
         pannellum: 'lib/pannellum',
         libpannellum: 'lib/libpannellum',
+        owl: 'lib/owl.carousel.min',
         'socket.io': 'lib/socket.io.min',
     }
 
 } );
 
-requirejs( [ 'main' ] );
+requirejs( [ 'jquery', 'main' ] );
