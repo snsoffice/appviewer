ef76bb6dd3584d0b954a7291da12d7ca59d07da8
Jondy Zhao <jondy.zhao@gmail.com>
2017-10-03 22:33:58 +0800

    Add handler

--- js/toolbox.js
+++ js/toolbox.js
@@ -9,19 +9,19 @@ define( [], function () {
         this.backdrop.innerHTML = 
             '<div class="dx-toolbox">' +
             '  <div class="dx-tool">' +
-            '    <a class="btn btn-lg btn-default" href="#">' +
+            '    <a class="btn btn-lg btn-default" href="#" data-trigger="take-photo">' +
             '      <i class="fa fa-camera fa-3x"></i>' +
             '      <br><span>拍照，记录生活</span>' +
             '    </a>' +
             '  </div>' +
             '  <div class="dx-tool">' +
-            '    <a class="btn btn-lg btn-default" href="#">' +
+            '    <a class="btn btn-lg btn-default" href="#" data-trigger="take-broadcast">' +
             '      <i class="fa fa-video-camera fa-3x"></i>' +
             '      <br><span>直播，展示风采</span>' +
             '    </a>' +
             '  </div>' +
             '  <div class="dx-tool">' +
-            '    <a class="btn btn-lg btn-default" href="#">' +
+            '    <a class="btn btn-lg btn-default" href="#" data-trigger="take-navigation">' +
             '      <i class="fa fa-road fa-3x"></i>' +
             '      <br><span>导航，指引人生</span>' +
             '    </a>' +
@@ -31,10 +31,12 @@ define( [], function () {
         this.backdrop.addEventListener( 'click', Toolbox.prototype.hide.bind( this ), false);
         document.body.append( this.backdrop );
 
-        // this.backdrop.querySelector( 'button' ).addEventListener( 'click', function (e) {
-        //     alert( 'button' );
-        //     e.stopPropagation();
-        // }, false );
+        var handler = function ( e ) {
+            document.dispatchEvent( new Event( e.currentTarget.getAttribute( 'data-trigger' ) ) );
+        };
+        Array.prototype.forEach.call( this.backdrop.querySelectorAll( 'a' ), function ( element ) {
+            element.addEventListener( 'click', handler );
+        } );
 
     }
 
