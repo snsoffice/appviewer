9c045a13c6f6ae472c0d19cbe4f97696da6919e8
Jondy Zhao <jondy.zhao@gmail.com>
2017-11-01 06:55:42 +0800

    Add modebar

---
+++ js/modebar.js
@@ -0,0 +1,64 @@
+define( [ 'dxbase' ],
+
+function( dxbase ) {
+
+    Modebar = function ( app, opt_options ) {
+        dxbase.Component.call( this );
+
+        var element = document.getElementById( 'modebar' );
+        this.element = element;
+
+        this.mode = '';
+        this.modeName = [];
+        this.modeList = [];
+
+        element.querySelector( '#modebar > button' ).addEventListener( 'click', function ( e ) {
+            e.preventDefault();
+            this.resetMode();
+            this.element.style.visibility = 'hidden';
+
+        }.bind( this ), false );
+
+    }
+    dxbase.inherits( Modebar, dxbase.Component );
+
+    Modebar.prototype.add = function ( name, component ) {
+        var index = this.modeName.indexOf( name );
+        if ( index === -1 ) {
+            this.modeName.push( name );
+            this.modeList.push( component );
+        }
+        else
+            this.modeList[ index ] = component;
+    };
+
+    Modebar.prototype.remove = function ( name ) {
+        if ( name === this.mode )
+            this.resetMode();
+        var index = this.modeName.indexOf( name );
+        if ( index > -1 ) {
+            this.modeName.splice( index, 1 );
+            this.modeList.splice( index, 1 );
+        }
+    };
+
+    Modebar.prototype.setMode = function ( name ) {
+        var index = this.modeName.indexOf( name );
+        if ( index > -1 && this.mode !== name ) {
+            this.resetMode();
+            this.mode = name;
+        }
+        return index;
+    };
+
+    Modebar.prototype.resetMode = function () {
+        var index = this.mode === '' ? -1 : this.modeName.indexOf( this.mode );
+        if ( index > -1 ) {
+            this.modeList[ index ].close()
+            this.mode = '';
+        }
+    };
+
+    return Modebar;
+
+} );
