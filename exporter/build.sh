2878207efdf6bfa0e0596bf83814a68fb8cd2497
Jondy Zhao <jondy.zhao@gmail.com>
2017-11-10 04:11:39 +0800

    Add build script

---
+++ exporter/build.sh
@@ -0,0 +1,1 @@
+javac -cp SweetHome3D-5.6.jar com/eteks/sweethome3d/plugin/exportjson/Exporter.java && javac -cp SweetHome3D-5.6.jar com/eteks/sweethome3d/plugin/exporter/*.java && zip -r exporter.jar com/
