2c52bf03f1559d3e373e1023aeabbc0cdfd2c27a
Jondy Zhao <jondy.zhao@gmail.com>
2017-12-11 09:50:55 +0800

    Fix encoding error

--- exporter/build.sh
+++ exporter/build.sh
@@ -1,1 +1,3 @@
-javac -cp SweetHome3D-5.6.jar com/eteks/sweethome3d/plugin/exportjson/Exporter.java && javac -cp SweetHome3D-5.6.jar com/eteks/sweethome3d/plugin/exporter/*.java && zip -r exporter.jar com/
+javac -encoding utf-8 -cp SweetHome3D-5.6.jar com/eteks/sweethome3d/plugin/exportjson/Exporter.java && \
+javac -encoding utf-8 -cp SweetHome3D-5.6.jar com/eteks/sweethome3d/plugin/exporter/*.java && \
+zip -r exporter.jar com/
