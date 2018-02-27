3dca33a02f568104dfaf979dbdd093fe9fe4e41c
Jondy Zhao <jondy.zhao@gmail.com>
2018-02-27 12:24:05 +0800

    Refine searchbox

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
-define( [ 'ifuture' ],
+define( [ 'ifuture', 'jquery' ],
 
-function( ifuture ) {
+function( ifuture, $ ) {
 
     Navbar = function ( app, opt_options ) {
 
@@ -8,21 +8,57 @@ function( ifuture ) {
 
         var element = document.getElementById( 'navbar' );
         this.element = element;
-        
-        element.querySelector( '#future-refresh' ).addEventListener( 'click', function ( e ) {
-            e.preventDefault();
-            // app.request( 'manager', 'show', 'toolbox' );
-        }, false );
+
+        var searchinput = element.querySelector( '#searchbox > input' );
+        var searchresult = [];
 
         element.querySelector( '#future-search' ).addEventListener( 'click', function ( e ) {
             e.preventDefault();
-            app.request( 'manager', 'show', 'search' );
+            searchinput.style.opacity = '0.6';
+            searchinput.focus();
+
+            if ( searchresult.length ) {
+                var html = [ '<ul class="list-group list-group-flush">' ];
+                for ( var i = 0 ; i < searchresult.length; i ++ ) {
+                    html.push( '<li class="list-group-item" index="' + i + '">' + searchresult[ i ] + '</li>' );
+                }                
+                html.push( '</ul>' );
+
+                var popover = document.createElement( 'DIV' );
+                popover.innerHTML = html.join('');
+
+                popover.addEventListener( 'click', function ( e ) {
+
+                    var target = e.target;
+                    while ( target && target.tagName.toUpper() !== 'LI' ) {
+                        target = target.parentElement;
+                    }
+
+                }, false );
+
+                $( searchinput ).popover( {
+                    content: popover,
+                    html: true,
+                    trigger: 'focus',
+                    placement: 'auto',
+                } ).popover( 'show' );
+            }
+
         }, false );
 
-        element.querySelector( '#future-menu' ).addEventListener( 'click', function ( e ) {
-            e.preventDefault();
-            // app.request( 'manager', 'show', 'talk' );
+        searchinput.addEventListener( 'blur', function ( e ) {
+            searchinput.style.opacity = '0';
         }, false );
+
+        // element.querySelector( '#future-refresh' ).addEventListener( 'click', function ( e ) {
+        //     e.preventDefault();
+        //     app.request( 'manager', 'show', 'toolbox' );
+        // }, false );
+
+        // element.querySelector( '#future-menu' ).addEventListener( 'click', function ( e ) {
+        //     e.preventDefault();
+        //     app.request( 'manager', 'show', 'talk' );
+        // }, false );
 
         // element.querySelector( '#show-manager' ).addEventListener( 'click', function ( e ) {
         //     e.preventDefault();
