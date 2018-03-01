09b7ee68a1a482c3bd3c6e819222ce17d206de46
Jondy Zhao <jondy.zhao@gmail.com>
2018-03-01 15:58:12 +0800

    Add polyfill for remove()

--- entry.js
+++ entry.js
@@ -1,3 +1,26 @@
+// 
+// Polyfills
+// 
+
+// polyfill the remove() method in Internet Explorer 9 and iOS Safari
+// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        if (this.parentNode !== null)
+          this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
 require.config( {
 
     baseUrl: 'js',
