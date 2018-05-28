e0e0a587b2f4f058d4ad03bc69fbc20c13c95b3d
Jondy Zhao <jondy.zhao@gmail.com>
2018-05-29 06:29:24 +0800

    Fxi timeout issue

--- entry.js
+++ entry.js
@@ -27,6 +27,20 @@ if (!String.prototype.startsWith) {
 	};
 }
 
+if (typeof Object.create !== "function") {
+    Object.create = function (proto, propertiesObject) {
+        if (!(proto === null || typeof proto === "object" || typeof proto === "function")) {
+            throw TypeError('Argument must be an object, or null');
+        }
+        var temp = new Object();
+        temp.__proto__ = proto;
+        if(typeof propertiesObject ==="object")
+            Object.defineProperties(temp,propertiesObject);
+        return temp;
+    };
+}
+
+
 require.config( {
 
     baseUrl: 'js',
@@ -45,7 +59,10 @@ require.config( {
         owl: 'lib/owl.carousel.min',
         videojs: 'lib/video.min',
         'socket.io': 'lib/socket.io.min',
-    }
+    },
+
+    // Fix timeout issue
+    waitSeconds: 0,
 
 } );
 
